7e2156a9c4e14c324642d750866542757992a809
Joshua Wulf <sitapati@worldsankirtan.net>
2015-05-11 21:57:42 +1000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var program = require('commander');
+
+program
+ .version('0.0.1')
+ .option('-n, --ninja-file [uid]', 'Ninja file UID')
+ .parse(process.argv);
+
+if (program.ninjaFile) {
+
+  var express = require('express');
+  var app = express();
+  var request = require('request');
+  var srcUrl = 'http://node-coding-ninja.s3-website-ap-southeast-2.amazonaws.com/'
+                + program.ninjaFile;
+
+  app.get('*',function(req,res){
+    console.log(req.url);
+    console.log(srcUrl + req.url);
+    request(srcUrl + req.url).pipe(res);
+  });
+
+  var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Ninja Dojo at http://%s:%s', host, port);
+
+  });
+}
